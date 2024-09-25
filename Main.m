8f2bbb81279e175f1ab0ff94c2c7891d1d245d57
Dreadfulandy <andrew.s.goode@student.uts.edu.au>
2024-09-25 14:30:20 +1000

    HitMeBot Work
    Updated HitMeBot to a 7DOF robot, need to finalise lengths and Qlims but the angles are all sorted.

--- Main.m
+++ Main.m
@@ -1,61 +1,83 @@
-%%Main
+classdef Main < handle
+%#ok<*NASGU>
+%#ok<*NOPRT>
+%#ok<*TRYNC>
+    properties
+        player % DoBot Robot Object
+        dealer % HitMeBot Object
+        HitMeHome % HitMeBot home q posiiton
+        DoBotHome % DoBotMagician home q position
+        
+    end
 
-clf
-clc
+    methods
+        %% Contructor
+        function self = Main()
+            clf
+            clc
+            hold on
+            self.Environment();
+            self.player = DobotMagician(transl(0,-0.2,0.45)*trotz(-pi/2));
+            self.dealer = HitMeBot(transl(0,-1.4,0.45)*trotz(pi/2));
+            qTest = [0,deg2rad(30),deg2rad(30),deg2rad(80),0];
+            self.player.model.animate(qTest);
+            drawnow();
+        end
+    end
 
-hold on
+    methods(Static)
+        %% Generate Environment
+        function Environment()
+            xLimits = [-2, 2];
+            yLimits = [-2, 2];
+            zLimits = [0, 2];
+            axis([xLimits, yLimits, zLimits]);
 
-xLimits = [-2, 2];
-yLimits = [-2, 2];
-zLimits = [0, 2];
-axis([xLimits, yLimits, zLimits]);
+            azimuth = 225;
+            elevation = 30;
+            view(azimuth, elevation)
 
-azimuth = 225;
-elevation = 30;
-view(azimuth, elevation)
+            blackjackTable = PlaceObject('blackjackTable.ply', [0, -0.6, -1.2]);
+            blackjackVerts = [get(blackjackTable,'Vertices'), ones(size(get(blackjackTable,'Vertices'),1),1)]*trotx(pi/2);
+            set(blackjackTable,'Vertices',blackjackVerts(:,1:3))
 
-blackjackTable = PlaceObject('blackjackTable.ply', [0, -0.6, -1.2]);
-blackjackVerts = [get(blackjackTable,'Vertices'), ones(size(get(blackjackTable,'Vertices'),1),1)]*trotx(pi/2);
-set(blackjackTable,'Vertices',blackjackVerts(:,1:3))
-
-stoolPositions = [0,0,-0.2;
+            stoolPositions = [0,0,-0.2;
                 0.423,0,-0.294;
                 -0.423,0,-0.294;
                 0.766,0,-0.557;
                 -0.766,0,-0.557;
                 0.966,0,-0.941;
-                -0.966,0,-0.941];
+                -0.966,0,-0.941;
+                0,0,-1.4];
 
-stools = cell(size(stoolPositions,1), 1);
+            stools = cell(size(stoolPositions,1), 1);
 
-for i = 1:size(stoolPositions,1)
-    stools{i} = PlaceObject('stool.ply', stoolPositions(i, :));
-    stoolVerts = [get(stools{i},'Vertices'), ones(size(get(stools{i},'Vertices'),1),1)]*trotx(pi/2);
-    set(stools{i},'Vertices',stoolVerts(:,1:3))
+            for i = 1:size(stoolPositions,1)
+                stools{i} = PlaceObject('stool.ply', stoolPositions(i, :));
+                stoolVerts = [get(stools{i},'Vertices'), ones(size(get(stools{i},'Vertices'),1),1)]*trotx(pi/2);
+                set(stools{i},'Vertices',stoolVerts(:,1:3))
+            end
+
+            surf([-2,-2;2,2] ...
+                ,[2,-2;2,-2] ...
+                ,[0,0;0,0] ...
+                ,'CData',imread('floor.jpg') ...
+                ,'FaceColor','texturemap');
+            surf([2,-2;2,-2] ...
+                ,[-2,-2;-2,-2] ...
+                ,[2,2;0,0] ...
+                ,'CData',imread('casinoWall.jpg') ...
+                ,'FaceColor','texturemap');
+            surf([2,2;2,2] ...
+                ,[2,-2;2,-2] ...
+                ,[2,2;0,0] ...
+                ,'CData',imread('darkRedBrickWall.jpg') ...
+                ,'FaceColor','texturemap');
+        end
+        function TestFunction(self)
+            q = [0,0,0,0,0,0];
+            self.dealer.model.teach(q)
+            %self.dealer.TestMoveHitMeBot()
+        end
+    end
 end
-
-surf([-2,-2;2,2] ...
-    ,[2,-2;2,-2] ...
-    ,[0,0;0,0] ...
-    ,'CData',imread('floor.jpg') ...
-    ,'FaceColor','texturemap');
-surf([2,-2;2,-2] ...
-    ,[-2,-2;-2,-2] ...
-    ,[2,2;0,0] ...
-    ,'CData',imread('casinoWall.jpg') ...
-    ,'FaceColor','texturemap');
-surf([2,2;2,2] ...
-    ,[2,-2;2,-2] ...
-    ,[2,2;0,0] ...
-    ,'CData',imread('darkRedBrickWall.jpg') ...
-    ,'FaceColor','texturemap');
-
-
-player = DobotMagician(transl(0,-0.2,0.45)*trotz(-pi/2));
-home = transl(0,0,1)
-dealer = HitMeBot(home);
-qTest = [0,deg2rad(30),deg2rad(30),deg2rad(80),0];
-player.model.animate(qTest);
-
-axis equal
-
--- Robots/@HitMeBot/HitMeBot.m
+++ Robots/@HitMeBot/HitMeBot.m
@@ -16,19 +16,20 @@ classdef HitMeBot < RobotBaseClass
 				self.model.base = self.model.base.T * baseTr;
             end
             self.name = 'HitMeBot'
-            self.homeQ  = [0,0,0,0,0,0];
+            self.homeQ  = [0,pi/4,0,-pi/4,0,0,0];
             self.PlotAndColourRobot();
             %self.animate(homeQ);
         end
 
 %% CreateModel
         function CreateModel(self)       
-            link(1) = Link('d',0,'a',0.1,'alpha',-pi/2,'offset',0,'qlim',[deg2rad(-180),deg2rad(180)]);
+            link(1) = Link('d',0.2,'a',0.1,'alpha',pi/2,'offset',0,'qlim',[deg2rad(-180),deg2rad(180)]);
             link(2) = Link('d',0,'a',0.1,'alpha',0,'offset',0,'qlim',[deg2rad(-180),deg2rad(180)]);
             link(3) = Link('d',0,'a',0.1,'alpha',0,'offset',0,'qlim',[deg2rad(-180),deg2rad(180)]);
             link(4) = Link('d',0,'a',0.1,'alpha',pi/2,'offset',0,'qlim',[deg2rad(-180),deg2rad(180)]);
-            link(5) = Link('d',0,'a',0.1,'alpha',0,'offset',0,'qlim',[deg2rad(-180),deg2rad(180)]);
-            link(6) = Link('d',0,'a',0.1,'alpha',0,'offset',0,'qlim',[deg2rad(-180),deg2rad(180)]);
+            link(5) = Link('d',0.1,'a',0,'alpha',-pi/2,'offset',0,'qlim',[deg2rad(-180),deg2rad(180)]);
+            link(6) = Link('d',0,'a',0,'alpha',pi/2,'offset',pi/2,'qlim',[deg2rad(-180),deg2rad(180)]);
+            link(7) = Link('d',0.1,'a',0,'alpha',0,'offset',0,'qlim',[deg2rad(-180),deg2rad(180)]);
 
             self.model = SerialLink(link,'name',self.name);
         end   
