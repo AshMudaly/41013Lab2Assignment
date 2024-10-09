c6ab9d0794d74658118d8ca2096e81e8663eb30a
Ash Mudaly <ashanthan.a.mudaly@student.uts.edu.au>
2024-10-09 13:53:43 +1100

    trying this>?

--- testinggame.m
+++ testinggame.m
@@ -1,3 +1,5 @@
 % In a new script or command window
 game = BlackjackTest();  % Create an instance of the BlackjackTest class
 game.play();             % Start the game
+% gui = guifunctionsample();
+% gui.play();
