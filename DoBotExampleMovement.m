8cd0ce02b767960378a0acc4700b53718b5f89f2
Ash Mudaly <ashanthan.a.mudaly@student.uts.edu.au>
2024-10-22 15:17:24 +1100

    Video footage + DoBot Magician Movement
    From the mechatronics lab, these are the respective videos and code for the movement for hit, check, stay, card and chip placement

--- DoBotExampleMovement.m
+++ DoBotExampleMovement.m
@@ -10,11 +10,157 @@ dobot = DobotMagicianRealMove();
 
 %% Test Motion
 %% Publish custom joint target
-joint_target = [0.0,0.4,0.3,0.0];
+%Chip Pickup Code
+chip_pickup = [0.7,0.83,1.4,0.0]
+chip_waypoint = [0.0,0.0,0.5,0.0];
+chip_placement = [0.0,1.2,0.7,0.0];
+
+dobot.PublishTargetJoint(chip_waypoint);
+pause(0.3)
 
 %joint_target2 = [0.0,0.0,1.39,0.0];
 %joint_target3 = [-0.32,0.0,0.89,2.2];
-dobot.PublishTargetJoint(joint_target);
+dobot.PublishTargetJoint(chip_pickup);
+
+onOff = 1;
+openClose = 1;
+dobot.PublishToolState(onOff,openClose);
+pause(0.3)
+% chip placement
+
+dobot.PublishTargetJoint(chip_waypoint);
+pause(0.3)
+dobot.PublishTargetJoint(chip_placement);
+pause(4.5)
+onOff = 0;
+openClose = 0;
+dobot.PublishToolState(onOff,openClose);
+
+dobot.PublishTargetJoint(chip_waypoint);
+%% 
+%Card Pickup
+
+card_pickup = [0.0,1.25,0.7,0.0]
+card_pickup2 = [0.2,1.25,0.7,0.0]
+card_waypoint = [0.0,0.0,0.5,0.0];
+
+dobot.PublishTargetJoint(card_waypoint);
+pause(0.5)
+
+%pickup
+dobot.PublishTargetJoint(card_pickup);
+pause(2.0)
+%suction on
+onOff = 1;
+openClose = 1;
+dobot.PublishToolState(onOff,openClose);
+
+pause(0.5)
+
+card_check = [0.0,0.2,0.6,1.5];
+dobot.PublishTargetJoint(card_check);
+pause(0.5)
+dobot.PublishTargetJoint(card_pickup);
+pause(4.0)
+
+onOff = 0;
+openClose = 0;
+dobot.PublishToolState(onOff,openClose);
+
+dobot.PublishTargetJoint(card_waypoint);
+pause(0.5)
+
+dobot.PublishTargetJoint(card_waypoint);
+pause(0.5)
+
+%pickup
+dobot.PublishTargetJoint(card_pickup2);
+pause(2.0)
+%suction on
+onOff = 1;
+openClose = 1;
+dobot.PublishToolState(onOff,openClose);
+
+pause(0.5)
+
+card_check = [0.0,0.2,0.6,1.5];
+dobot.PublishTargetJoint(card_check);
+pause(0.5)
+dobot.PublishTargetJoint(card_pickup2);
+pause(4.0)
+
+onOff = 0;
+openClose = 0;
+dobot.PublishToolState(onOff,openClose);
+
+dobot.PublishTargetJoint(card_waypoint);
+pause(0.5)
+
+%% 
+%Hit me
+
+hitMe_waypoint = [0.0,0.0,0.5,0.0];
+hitMe_high = [0.0,1.2,0.7,0.0];
+hitMe_low = [0.0,1.25,0.72,0.0];
+
+dobot.PublishTargetJoint(hitMe_waypoint);
+pause(0.3);
+
+dobot.PublishTargetJoint(hitMe_high);
+pause(0.3);
+
+dobot.PublishTargetJoint(hitMe_low);
+pause(0.3);
+
+dobot.PublishTargetJoint(hitMe_high);
+pause(0.3);
+
+dobot.PublishTargetJoint(hitMe_low);
+pause(0.3);
+
+dobot.PublishTargetJoint(hitMe_waypoint);
+pause(0.3)
+
+%% 
+%stay
+
+stay_waypoint = [0.0,0.0,0.5,0.0];
+stay_left = [-0.05,1.1,0.7,0.0];
+stay_right = [0.05,1.1,0.7,0.0];
+
+dobot.PublishTargetJoint(stay_waypoint);
+pause(0.3);
+
+dobot.PublishTargetJoint(stay_left);
+pause(0.3);
+
+dobot.PublishTargetJoint(stay_right);
+pause(0.3);
+
+dobot.PublishTargetJoint(stay_left);
+pause(0.3);
+
+dobot.PublishTargetJoint(stay_right);
+pause(0.3);
+
+dobot.PublishTargetJoint(stay_left);
+pause(0.3);
+
+dobot.PublishTargetJoint(stay_waypoint);
+pause(0.3);
+
+
+%% chip stacking
+chip_placement1 = [0.7,0.83,1.4,0.0];
+chip_waypoint = [0.0,0.0,0.5,0.0];
+chip_pickup1 = [0.0,1.2,0.7,0.0];
+
+chip_placement2 = [0.7,0.80,1.4,0.0]
+chip_pickup2 = [0.1,1.2,0.7,0.0];
+
+dobot.PublishTargetJoint(chip_waypoint);
+pause(0.3)
+
 %%
 test_Joint = [0,0.5,0.6,0.0];
 dobot.PublishTargetJoint(test_Joint);
