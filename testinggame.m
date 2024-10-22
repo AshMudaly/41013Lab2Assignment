5faedd9617a7cf90c1b933ab1a3bd5f7f4d66682
Ash Mudaly <ashanthan.a.mudaly@student.uts.edu.au>
2024-10-22 17:45:59 +1100

    gui is somwjhat wthorking
    game can initialise and run, spamming the buttons does reshuffle deck (which shows that works) and then partially gets into a game. will wokr on that later today/tmr

--- testinggame.m
+++ testinggame.m
@@ -2,9 +2,25 @@
 clc;
 clear all;
 close; 
-
+% 
 g = BlackjackTest();  % Create an instance of the BlackjackTest class
 g.play();             % Start the game
 
 % gui = guifunctionsample();
 % gui.play();
+% Initialization and deal initial hands
+% function startupFcn(app)
+%     app.BlackjackGame = BlackjackTest();  % Initialize Blackjack game
+%     app.BlackjackGame.balance = 100;  % Set starting balance
+%     app.bet = 10;  % Set default bet
+%     app.playerBust = false;
+% 
+%     % Deal initial hands
+%     [app.playerHand, app.BlackjackGame] = app.BlackjackGame.dealCard([], 2);
+%     [app.dealerHand, app.BlackjackGame] = app.BlackjackGame.dealCard([], 2);
+% 
+%     % Update balance and game info display
+%     app.updateBalance();
+%     app.updateGameInfo();
+% end
+
