8c50ab45765d0ad898bc5a78d39f90fa73741c9b
Ash Mudaly <ashanthan.a.mudaly@student.uts.edu.au>
2024-10-29 12:52:26 +1100

    Reset

--- BlackjackTest.m
+++ BlackjackTest.m
@@ -1,43 +1,37 @@
 classdef BlackjackTest
+    %#ok<*NASGU>
+    %#ok<*NOPRT>
+    %#ok<*TRYNC>
     properties
         balance;   % Player's starting balance
         deck;      % Deck of cards
         cardNames; % Card names
-        GuiTesting; % Reference to the GUI app instance
     end
 
     methods
         % Constructor to initialize properties
-        function obj = BlackjackTest(GuiTestingInstance)
-            obj.GuiTesting = GuiTestingInstance; % Store reference to app for GUI output
-            obj.balance = 100;
+        function obj = BlackjackTest()
+            disp('Welcome to Blackjack!');
+            pause(2);
+            obj.balance = 100;  % Player's starting balance
             obj.cardNames = {'Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'};
-            obj = obj.shuffleDeck();
-            obj.GuiTesting.updateOutput('Welcome to Blackjack!');
+            obj = obj.shuffleDeck();  % Shuffle the deck at the start
         end
-        
-        % Function to shuffle the deck
-        function obj = shuffleDeck(obj)
-            deckValues = [1:10, 10, 10, 10];
-            obj.deck = repmat(deckValues, 1, 4);
-            obj.deck = obj.deck(randperm(length(obj.deck)));
-            obj.GuiTesting.updateOutput('Deck shuffled!');
-        end
-        
+
         % Function to start the game
         function play(obj)
             while obj.balance > 0
-                obj.GuiTesting.updateOutput(['Current balance: $', num2str(obj.balance)]);
-                % Wait for bet to be entered in the GUI
-                obj.GuiTesting.waitForBet();
-                bet = obj.GuiTesting.betAmount;  % Get bet amount from the GUI
-                obj.GuiTesting.updateOutput(['You bet $', num2str(bet)]);
-                
+                clc;
+                disp(['Current balance: $', num2str(obj.balance)]);
+
+                bet = obj.getBet();  % Betting amount
+                disp(['You bet $', num2str(bet)]);
+
                 % Shuffle deck if fewer than 15 cards are left
                 if length(obj.deck) < 15
                     obj = obj.shuffleDeck();
                 end
-                
+
                 % Player's and Dealer's hands
                 [playerHand, obj] = obj.dealCard([], 2);
                 [dealerHand, obj] = obj.dealCard([], 2);
@@ -45,73 +39,138 @@ classdef BlackjackTest
 
                 % Player's turn
                 [playerBust, obj] = obj.playerTurn(playerHand, dealerHand, bet);
-                
+
                 % Dealer's turn if player hasn't busted
                 if ~playerBust
                     obj = obj.dealerTurn(playerHand, dealerHand, bet);
                 end
 
-                % Ask if the player wants to continue via the GUI
+                % Ask if the player wants to continue
                 if obj.balance > 0
-                    obj.GuiTesting.updateOutput('Do you want to play again?');
-                    obj.GuiTesting.waitForPlayAgainDecision();  % Wait for player decision (y/n)
-                    if ~obj.GuiTesting.playAgain
-                        break;  % Exit the game
+                    cont = input('Do you want to play again? (y/n): ', 's');
+                    switch cont
+                        case 'y'
+                            continue;  % Continue playing
+                        case 'n'
+                            break;  % Exit the game
+                        otherwise
+                            disp('Invalid input. Exiting game.');
+                            break;  % Exit the game
                     end
                 else
-                    obj.GuiTesting.updateOutput('You have run out of money! Game over.');
+                    disp('You have run out of money! Game over.');
                 end
             end
         end
 
-        % Function to handle player's turn
+        % Shuffle the deck and ensure it resets to 52 cards
+        function obj = shuffleDeck(obj)
+            deckValues = [1:10, 10, 10, 10];  % J, Q, K represented by 10, Ace as 1
+            obj.deck = repmat(deckValues, 1, 4);  % 4 suits (total 52 cards)
+            obj.deck = obj.deck(randperm(length(obj.deck)));  % Shuffle the deck
+
+            % Debugging: Check the length of the deck after shuffling
+            disp(['Deck size after shuffling: ', num2str(length(obj.deck))]);
+        end
+
+        % Helper function to get player's bet
+        function bet = getBet(obj)
+            while true
+                betInput = input(['Enter your bet (1 to ', num2str(obj.balance), '): '], 's'); % Take input as a string
+                bet = str2double(betInput);  % Convert input to a number
+
+                switch true
+                    case isnan(bet) || bet < 1 || bet > obj.balance
+                        disp(['Invalid input. Please enter a value between 1 and ', num2str(obj.balance), '.']);
+                        disp(['Current balance: $', num2str(obj.balance)]); % Display current balance
+                        pause(2);
+                        clc;
+                    otherwise
+                        return;  % Valid input, exit the loop
+                end
+            end
+        end
+
+        % Helper function to deal a card and update the deck
+        function [hand, obj] = dealCard(obj, hand, numCards)
+            if nargin < 3
+                numCards = 1; % Default to dealing 1 card
+            end
+            numCards = min(numCards, length(obj.deck));  % Ensure we don't deal more cards than available
+            hand = [hand, obj.deck(1:numCards)];  % Add card(s) to hand
+            obj.deck(1:numCards) = [];  % Remove card(s) from deck
+
+            % Debugging: Check deck size after dealing cards
+            disp(['Deck size after dealing: ', num2str(length(obj.deck))]);
+
+            if isempty(obj.deck)  % Reshuffle if deck is empty
+                disp('Reshuffling deck...');
+                obj = obj.shuffleDeck();
+            end
+        end
+
+        % Helper function to handle player's turn
         function [playerBust, obj] = playerTurn(obj, playerHand, dealerHand, bet)
             playerBust = false;
-            obj.GuiTesting.updateOutput('Player''s turn.');
 
             while true
+                clc;
                 obj.showHands(playerHand, dealerHand, true);
 
-                % Wait for the player's decision (hit or stand) via the GUI
-                obj.GuiTesting.waitForPlayerDecision();
-                if obj.GuiTesting.PlayerChoseHit
-                    [playerHand, obj] = obj.dealCard(playerHand);
-                    if obj.calculateTotal(playerHand) > 21
-                        obj.GuiTesting.updateOutput('Bust! You lose.');
-                        obj.balance = obj.balance - bet;
-                        obj.GuiTesting.updateOutput(['New balance: $', num2str(obj.balance)]);
-                        playerBust = true;  % Player busted
-                        return;  % Exit the player turn
-                    end
-                elseif obj.GuiTesting.PlayerChoseStand
-                    break;  % Exit the player turn
+                if length(playerHand) >= 5
+                    disp('You have 5 cards. You must stand.');
+                    break;
+                end
+
+                choice = input('Do you want to (h)it or (s)tand? ', 's');
+                switch choice
+                    case 'h'
+                        [playerHand, obj] = obj.dealCard(playerHand);
+                        if obj.calculateTotal(playerHand) > 21
+                            clc;
+                            obj.showHands(playerHand, dealerHand, true);
+                            disp('Bust! You lose.');
+                            obj.balance = obj.balance - bet;
+                            disp(['New balance: $', num2str(obj.balance)]);
+                            playerBust = true;  % Player busted
+                            return;  % Exit the player turn
+                        end
+                    case 's'
+                        break;  % Exit the player turn
+                    otherwise
+                        disp('Invalid choice. Please type h to hit or s to stand.');
                 end
             end
         end
 
-        % Function to handle dealer's turn
+
         function [obj] = dealerTurn(obj, playerHand, dealerHand, bet)
-            dealerTotal = obj.calculateTotal(dealerHand);
-            obj.GuiTesting.updateOutput(['Dealer''s cards: ', obj.handToString(dealerHand), ' | Total: ', num2str(dealerTotal)]);
+            clc;
+            dealerTotal = obj.calculateTotal(dealerHand);  % Calculate the initial dealer total
+            disp(['Dealer''s cards: ', obj.handToString(dealerHand), ' | Total: ', num2str(dealerTotal)]);
 
             % Dealer draws cards until total is at least 17
             while dealerTotal < 17
-                [dealerHand, obj] = obj.dealCard(dealerHand);
-                dealerTotal = obj.calculateTotal(dealerHand);
-                obj.GuiTesting.updateOutput(['Dealer draws a card. Dealer''s cards: ', obj.handToString(dealerHand), ' | Total: ', num2str(dealerTotal)]);
+                [dealerHand, obj] = obj.dealCard(dealerHand);  % Dealer draws a card
+                dealerTotal = obj.calculateTotal(dealerHand);  % Recalculate total after drawing
+                clc;
+                disp(['Dealer draws a card. Dealer''s cards: ', obj.handToString(dealerHand), ' | Total: ', num2str(dealerTotal)]);
             end
 
-            % Determine if the dealer busts
+            % Check if dealer busts
             if dealerTotal > 21
-                obj.GuiTesting.updateOutput('Dealer busts! You win.');
-                obj.balance = obj.balance + bet;  % Player wins
+                disp('Dealer busts! You win.');
+                obj.balance = obj.balance + bet;  % Player wins, increase balance
             else
-                % Determine the winner
+                % Determine the winner if dealer doesn't bust
                 balanceChange = obj.determineWinner(playerHand, dealerHand, bet);
                 obj.balance = obj.balance + balanceChange;
             end
-            obj.GuiTesting.updateOutput(['New balance: $', num2str(obj.balance)]);
-        end  % Corrected the placement of this end
+
+            % Display updated balance
+            disp(['New balance: $', num2str(obj.balance)]);
+        end
+
 
         % Helper function to calculate hand total
         function total = calculateTotal(~, hand)
@@ -130,11 +189,11 @@ classdef BlackjackTest
 
         % Helper function to show hands
         function showHands(obj, playerHand, dealerHand, hideDealer)
-            obj.GuiTesting.updateOutput(['Your cards: ', obj.handToString(playerHand), ' | Total: ', num2str(obj.calculateTotal(playerHand))]);
+            disp(['Your cards: ', obj.handToString(playerHand), ' | Total: ', num2str(obj.calculateTotal(playerHand))]);
             if hideDealer
-                obj.GuiTesting.updateOutput(['Dealer''s first card: ', obj.cardNames{dealerHand(1)}]);
+                disp(['Dealer''s first card: ', obj.cardNames{dealerHand(1)}]);
             else
-                obj.GuiTesting.updateOutput(['Dealer''s cards: ', obj.handToString(dealerHand), ' | Total: ', num2str(obj.calculateTotal(dealerHand))]);
+                disp(['Dealer''s cards: ', obj.handToString(dealerHand), ' | Total: ', num2str(obj.calculateTotal(dealerHand))]);
             end
         end
 
@@ -144,15 +203,16 @@ classdef BlackjackTest
             dealerTotal = obj.calculateTotal(dealerHand);  % Calculate dealer total
 
             if playerTotal > dealerTotal
-                obj.GuiTesting.updateOutput('You win!');
+                disp('You win!');
                 balanceChange = bet;  % Win the bet
             elseif playerTotal < dealerTotal
-                obj.GuiTesting.updateOutput('Dealer wins.');
+                disp('Dealer wins.');
                 balanceChange = -bet;  % Lose the bet
             else
-                obj.GuiTesting.updateOutput('It''s a tie!');
+                disp('It''s a tie!');
                 balanceChange = 0;  % No change in balance
             end
         end
+
     end
 end
