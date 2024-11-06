67612348e66a918e25a8124387b7b1e2c5f89262
Ash Mudaly <ashanthan.a.mudaly@student.uts.edu.au>
2024-11-06 15:17:07 +1100

    gui e stop

--- Controller.m
+++ Controller.m
@@ -161,6 +161,32 @@ classdef Controller < handle
 
         end
 
+        %% Gui E Stop
+        function EStop(self)
+            % Check E-stop status from GUI button state
+            while self.stop == 1
+                fprintf('STOP\n');
+                configureCallback(self.serialObj, "terminator", @(src, event) self.ReadData(src, event));
+                pause(0.1);
+
+                % Check if stop is disengaged
+                if self.stop == 0
+                    fprintf('Resuming operations\n');
+                    configureCallback(self.serialObj, "terminator", @(src, event) self.ReadData(src, event));
+                    break;
+                end
+            end
+        end
+
+        % New function to set stop state from GUI
+        function setGuiEStop(self, state)
+            self.stop = state; % Update stop state based on GUI toggle
+            if self.stop == 1
+                fprintf('GUI E-Stop engaged\n');
+            else
+                fprintf('GUI E-Stop disengaged\n');
+            end
+        end
         %% Get Joint Values - Return the join value as q values, 1 x 7 matrix or 1 x 5 matrix depending on the robot specified
         % Called inside Controller using "self.GetJointState(self.player/dealer)"
         % Called inside Main using "self.robots.GetJointState(self.robots.player/dealer)"
@@ -428,7 +454,7 @@ classdef Controller < handle
             % title('Manipulability')
 
         end
-
+        %% Return To Home RMRC
         function ReturnToHomeRMRCDealer(self)
             target = self.dealer.model.fkine(self.dealer.homeQ).T;
             currentT = self.GetPos(self.dealer);
@@ -475,45 +501,45 @@ classdef Controller < handle
             q0 = zeros(1,7);
             qMatrix = zeros(steps,7);       % Array for joint anglesR
             qdot = zeros(steps,7);          % Array for joint velocities                                                            % Initial guess for joint angles
-            qMatrix(1,:) = self.dealer.model.ikcon(T,q0); 
-                for i = 1:steps-1
-                    % UPDATE: fkine function now returns an SE3 object. To obtain the
-                    % Transform Matrix, access the variable in the object 'T' with '.T'.
-                    T = self.dealer.model.fkine(qMatrix(i,:)).T;                                         % Get forward transformation at current joint state
-                    deltaX = xyz(:,i+1) - T(1:3,4);                                         	% Get position error from next waypoint
-                    Rd = rpy2r(theta(1,i+1),theta(2,i+1),theta(3,i+1));                     % Get next RPY angles, convert to rotation matrix
-                    Ra = T(1:3,1:3);                                                        % Current end-effector rotation matrix
-                    Rdot = (1/deltaT)*(Rd - Ra);                                            % Calculate rotation matrix error
-                    S = Rdot*Ra';                                                           % Skew symmetric!
-                    linear_velocity = (1/deltaT)*deltaX;
-                    angular_velocity = [S(3,2);S(1,3);S(2,1)];                              % Check the structure of Skew Symmetric matrix!!
-                    deltaTheta = tr2rpy(Rd*Ra');                                            % Convert rotation matrix to RPY angles
-                    xdot = W*[linear_velocity;angular_velocity];                          	% Calculate end-effector velocity to reach next waypoint.
-                    J = self.dealer.model.jacob0(qMatrix(i,:));                             % Get Jacobian at current joint state
-                    m(i) = sqrt(det(J*J'));
-                    if m(i) < epsilon  % If manipulability is less than given threshold
-                        lambda = (1 - m(i)/epsilon)*5E-2;
-                    else
-                        lambda = 0;
-                    end
-                    invJ = inv(J'*J + lambda *eye(7))*J';                                   % DLS Inverse
-                    qdot(i,:) = (invJ*xdot)';                                               % Solve the RMRC equation (you may need to transpose the         vector)
-                    for j = 1:7                                                             % Loop through joints 1 to 6
-                        if qMatrix(i,j) + deltaT*qdot(i,j) < self.dealer.model.qlim(j,1)                 % If next joint angle is lower than joint limit...
-                            qdot(i,j) = 0; % Stop the motor
-                        elseif qMatrix(i,j) + deltaT*qdot(i,j) > self.dealer.model.qlim(j,2)             % If next joint angle is greater than joint limit ...
-                            qdot(i,j) = 0; % Stop the motor
-                        end
-                    end
-                    qMatrix(i+1,:) = qMatrix(i,:) + deltaT*qdot(i,:);                       % Update next joint state based on joint velocities
-                    positionError(:,i) = xyz(:,i+1) - T(1:3,4);                               % For plotting
-                    angleError(:,i) = deltaTheta;                                           % For plotting
+            qMatrix(1,:) = self.dealer.model.ikcon(T,q0);
+            for i = 1:steps-1
+                % UPDATE: fkine function now returns an SE3 object. To obtain the
+                % Transform Matrix, access the variable in the object 'T' with '.T'.
+                T = self.dealer.model.fkine(qMatrix(i,:)).T;                                         % Get forward transformation at current joint state
+                deltaX = xyz(:,i+1) - T(1:3,4);                                         	% Get position error from next waypoint
+                Rd = rpy2r(theta(1,i+1),theta(2,i+1),theta(3,i+1));                     % Get next RPY angles, convert to rotation matrix
+                Ra = T(1:3,1:3);                                                        % Current end-effector rotation matrix
+                Rdot = (1/deltaT)*(Rd - Ra);                                            % Calculate rotation matrix error
+                S = Rdot*Ra';                                                           % Skew symmetric!
+                linear_velocity = (1/deltaT)*deltaX;
+                angular_velocity = [S(3,2);S(1,3);S(2,1)];                              % Check the structure of Skew Symmetric matrix!!
+                deltaTheta = tr2rpy(Rd*Ra');                                            % Convert rotation matrix to RPY angles
+                xdot = W*[linear_velocity;angular_velocity];                          	% Calculate end-effector velocity to reach next waypoint.
+                J = self.dealer.model.jacob0(qMatrix(i,:));                             % Get Jacobian at current joint state
+                m(i) = sqrt(det(J*J'));
+                if m(i) < epsilon  % If manipulability is less than given threshold
+                    lambda = (1 - m(i)/epsilon)*5E-2;
+                else
+                    lambda = 0;
                 end
-                for i =1:steps
+                invJ = inv(J'*J + lambda *eye(7))*J';                                   % DLS Inverse
+                qdot(i,:) = (invJ*xdot)';                                               % Solve the RMRC equation (you may need to transpose the         vector)
+                for j = 1:7                                                             % Loop through joints 1 to 6
+                    if qMatrix(i,j) + deltaT*qdot(i,j) < self.dealer.model.qlim(j,1)                 % If next joint angle is lower than joint limit...
+                        qdot(i,j) = 0; % Stop the motor
+                    elseif qMatrix(i,j) + deltaT*qdot(i,j) > self.dealer.model.qlim(j,2)             % If next joint angle is greater than joint limit ...
+                        qdot(i,j) = 0; % Stop the motor
+                    end
+                end
+                qMatrix(i+1,:) = qMatrix(i,:) + deltaT*qdot(i,:);                       % Update next joint state based on joint velocities
+                positionError(:,i) = xyz(:,i+1) - T(1:3,4);                               % For plotting
+                angleError(:,i) = deltaTheta;                                           % For plotting
+            end
+            for i =1:steps
                 self.dealer.model.animate(qMatrix(i,:));
                 drawnow();
                 pause(0.1)
-                end
+            end
         end
     end
 
